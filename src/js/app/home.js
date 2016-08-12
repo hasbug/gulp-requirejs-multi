4f41bfacba12e0d2a1eee064e43588d192a7d2c4
hasbug <hinong@qq.com>
2016-08-12 17:42:35 +0800

    fullpage background

--- src/js/app/home.js
+++ src/js/app/home.js
@@ -1,5 +1,7 @@
 require(['../lib/jquery.fullPage'],function (fullpage) {
     $(document).ready(function() {
-        $('#fullpage').fullpage();
+        $('#fullpage').fullpage({
+            sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
+        });
     });
 });
